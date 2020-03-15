41601e10b28c3da6a96fc5a08e768cc4766a9527
ElenaRadiuk <olena.radiuk@gmail.com>
2020-03-15 23:15:40 +0200

    shorcode next game on SP s1

--- template-events.php
+++ template-events.php
@@ -17,6 +17,46 @@
       <?php endwhile; ?>
     <?php endif; ?>
 
+  <?php
+    echo '$page->event_name';
+    global $wpdb;
+    $pages = $wpdb->get_results(
+    "
+    SELECT event_name, post_content 
+    FROM $wpdb->posts
+    WHERE post_status = 'publish' 
+    AND post_type = 'event'
+    "
+      );
+   var_dump($pages);
+      /* вытаскивает из базы данных заголовки и содержимое
+      всех опубликованных страниц */
+      if( $pages ) {
+        foreach ( $pages as $page ) {
+          setup_postdata( $page );
+          echo $page->event_name;
+          echo '$page->event_name2';
+        }
+      }
+
+
+    $fivesdrafts = $wpdb->get_results(
+      "
+      SELECT ID, post_title 
+      FROM $wpdb->posts
+      WHERE post_status = 'publish'
+        AND post_type = 'event'
+      "
+    );
+
+    foreach ( $fivesdrafts as $fivesdraft )
+    {
+      echo $fivesdraft->post_title;
+    }
+  ;?>
+
+
+
 	</main>
 
 <?php get_footer(); ?>
